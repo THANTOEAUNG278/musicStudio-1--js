ae0c85655938d7996a9d5638377f4773c6b3ff1d
THANTOEAUNG <kyawshein034@gmail.com>
2023-10-20 21:39:58 +0900

    musicstudio-1

---
+++ script.js
@@ -0,0 +1,27 @@
+const playListContainer = document.getElementsByClassName('play-list-container')[0];
+
+const audioTag = document.getElementsByClassName('audioTag')[0]
+
+const musicPlay = [
+  {musicId: 'music/Akon-Beautiful.mp3', title: 'Beautiful - Akon' },
+  {musicId: 'music/Right Now-Akon .mp3', title: 'Right Now - Akon' },
+  {musicId: 'music/Toosii - Favorite Song .mp3', title: 'Favorite Song - Toosii ' },
+  {musicId: 'music/程响 - Hurt Inside .mp3', title: 'Hurt Inside- 程响 ' },
+]
+for (let i= 0; i < musicPlay.length; i++) {
+  const musicTag = document.createElement('div');
+  musicTag.addEventListener('click', () => {
+    musicId = musicPlay[i].musicId
+    audioTag.src = musicId
+  })
+  musicTag.classList.add('box')
+  const title = document.createTextNode((i + 1).toString() + '.' + musicPlay[i].title);
+  musicTag.appendChild(title)
+  playListContainer.appendChild(musicTag)
+}
+
+
+
+
+// const title = (i + 1).toString() + '.' + musicPlay[i].title;
+// musicTag.textContent = title
